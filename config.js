6e67efdeda646494eb537d8385d2e92c26d67f4c
hoang22042006 <102609067+hoang22042006@users.noreply.github.com>
2022-03-29 22:27:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Anh biết mà và Chúc Hạnh ngủ ngon nè😘❤️',
+    mess: 'Anh biết mà 🥰. Yêu Hạnh nhiều nhiều 😘😘',
+    messDesc: 'Chúc Hạnh ngủ ngon nè😘❤️',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
